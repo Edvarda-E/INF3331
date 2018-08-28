9699dbe3585d422e17aaffc3c9e01c282575911f
Edvarda-E <edvarda.eriksen@outlook.com>
2018-08-28 13:18:52 +0200

    Added comments and updated variable names for readability, removed -x

--- assignment2/climb.sh
+++ assignment2/climb.sh
@@ -1,22 +1,25 @@
-#!/bin/bash -x
+#!/bin/bash
 
 function climb () {
+    # If more than one positional parameter is passed, the function will return
+    # with an error code
     if [ "$#" -ge "2" ]; then
         echo "ERROR: This function can only handle one positional parameter"
         return 1
     fi
 
+    # Sets ARG1 to be $1, with the default value of 1 if $1 is unset or null
     ARG1=${1:-1}
     if [ "$ARG1" == "1" ]; then
         echo "Walking up one directory"
         cd ..
     else
-        foo="cd "
-        for (( i=0; i<$1; i++ ))
+        walkPath="cd "
+        for (( i=0; i<$ARG1; i++ ))
             do
-                foo+="../"
+                walkPath+="../"
             done
         echo "Walking up ${ARG1} directories"
-        $foo
+        $walkPath
     fi
 }
