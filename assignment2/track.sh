47a414042d047c0532d2d5572094d8bc170b1921
Edvarda-E <edvarda.eriksen@outlook.com>
2018-08-30 14:14:59 +0200

    Added functionality for start, status and stop, finishing basic functionality for task 2.2. Fine tuning still needed

--- assignment2/track.sh
+++ assignment2/track.sh
@@ -2,15 +2,36 @@
 
 function track () {
     action=$1
+    LOGFILE=./log.txt
+    lastLineOfFile=$(tail -1 "$LOGFILE")
     case "$action" in
         start)
-            echo "Start passed"
+            if [[ "${lastLineOfFile}" =~ (LABEL ){1}(.*) ]]; then
+                echo "ERROR: Another task is already running. Run track stop to end it."
+            elif [ "$#" -le "1" ]; then
+                echo "ERROR: Please provide a label as follows: track start \"label\""
+            else
+                if [[ "${lastLineOfFile}" =~ (END ){1}(.*) ]]; then
+                    echo -e "\r\n\r\n" >> $LOGFILE
+                fi
+                currDateTime=`date +"%c"`
+                echo -e "START ${currDateTime}\r\nLABEL ${2}" | tee -a $LOGFILE
+            fi
             ;;
         stop)
-            echo "Stop passed"
+            if [[ "${lastLineOfFile}" =~ (LABEL ){1}(.*) ]]; then
+                currDateTime=`date +"%c"` 
+                echo -e "\r\nEND ${currDateTime}" | tee -a $LOGFILE
+            else
+                echo "ERROR: No task is currently active"
+            fi
             ;;
         status)
-            echo "Status passed"
+            if [[ "${lastLineOfFile}" =~ (LABEL ){1}(.*) ]]; then
+                echo "Your current task is ${lastLineOfFile}"
+            else
+                echo "ERROR: No task is currently active"
+            fi
             ;;
         *)
             echo "ERROR: track only accepts the parameters start, stop and status"
