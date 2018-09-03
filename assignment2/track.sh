5f91f43b204345946d812a01b68ca28f4cec3c8b
Edvarda-E <edvarda.eriksen@outlook.com>
2018-09-03 15:55:34 +0200

    Implemnted track log, solving task 2.3

--- assignment2/track.sh
+++ assignment2/track.sh
@@ -11,16 +11,16 @@ function track () {
                 echo "ERROR: Please provide a label as follows: track start \"label\""
             else
                 if [[ "${lastLineOfFile}" =~ (END ){1}(.*) ]]; then
-                    echo -e "\r\n\r\n" >> $LOGFILE
+                    echo -e "\r\n" >> $LOGFILE
                 fi
-                currDateTime=`date +"%c"`
+                currDateTime=`date "+%d/%m/%Y %H:%M:%S"`
                 echo -e "START ${currDateTime}\r\nLABEL ${2}" | tee -a $LOGFILE
             fi
             ;;
         stop)
             if [[ "${lastLineOfFile}" =~ (LABEL ){1}(.*) ]]; then
-                currDateTime=`date +"%c"` 
-                echo -e "\r\nEND ${currDateTime}" | tee -a $LOGFILE
+                currDateTime=`date "+%d/%m/%Y %H:%M:%S"` 
+                echo -e "END ${currDateTime}" | tee -a $LOGFILE
             else
                 echo "ERROR: No task is currently active"
             fi
@@ -31,6 +31,28 @@ function track () {
             else
                 echo "ERROR: No task is currently active"
             fi
+            ;;
+        log)
+            while IFS='' read -r line || [[ -n $line ]]; do
+                if [[ "${line}" =~ (START ){1}(.*) ]]; then
+                    startTime="${line#* }"
+                    removeNewline="${startTime/$'\r'/}"
+                    startTimeConverted=`date -d "${removeNewline}" "+%T"`
+                elif [[ "${line}" =~ (LABEL ){1}(.*) ]]; then
+                    currentLabel="${line#* }"
+                elif [[ "${line}" =~ (END ){1}(.*) ]]; then
+                    endTime="${line#* }"
+                    removeNewline="${endTime/$'\r'/}"
+                    endTimeConverted=`date -d "${removeNewline}" "+%T"`
+
+                    startTimeInSeconds=`date +%s -d ${startTimeConverted}`
+                    endTimeInSeconds=`date +%s -d ${endTimeConverted}`
+                    differenceInSeconds=`expr ${endTimeInSeconds} - ${startTimeInSeconds}`
+                    finalTime=`date +%H:%M:%S -ud @${differenceInSeconds}`
+
+                    echo "Task ${currentLabel}: ${finalTime}"
+                fi
+            done < $LOGFILE
             ;;
         *)
             echo "ERROR: track only accepts the parameters start, stop and status"
