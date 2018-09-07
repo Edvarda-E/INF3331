4e6a8fcd3497710095964353eb1d426b8aac4e3a
Edvarda-E <edvarda.eriksen@outlook.com>
2018-09-07 13:21:15 +0200

    Implemented functionality to print * and *.py files

---
+++ assignment3/README.md
@@ -0,0 +1,13 @@
+# Assignment 3
+This is the directory for assignment 3, done by Edvarda Eriksen (ererikse).
+
+## 3.1 - Wordcount
+Running the script
+```
+$ chmod a+x wc.py
+$ ./wc.py ./example.txt
+```
+
+**Additional decisions**
+* Currently including `\n` in the character count, resulting in more characters than "conventional" character trackers.
+* Empty lines at the end of a file does not count
--- assignment3/wc.py
+++ assignment3/wc.py
@@ -2,17 +2,29 @@
 import sys
 import os
 
-fileName = os.path.basename(sys.argv[1])
-file = open(sys.argv[1], 'r')
-lineCount = 0
-wordCount = 0
-charCount = 0
 
-for line in file.readlines():
-    lineCount += 1
-    print(len(line))
-    charCount += len(line)
-    for word in line.split():
-        wordCount += 1
-file.close()
-print(lineCount, wordCount, charCount, fileName)
+def count_and_print(file):
+    fileName = os.path.basename(file)
+    file = open(file, 'r')
+    lineCount = 0
+    wordCount = 0
+    charCount = 0
+
+    for line in file.readlines():
+        lineCount += 1
+        # print(len(line))
+        charCount += len(line)
+        for word in line.split():
+            wordCount += 1
+    file.close()
+    print(lineCount, wordCount, charCount, fileName)
+
+
+if len(sys.argv) == 2 and os.path.isfile(sys.argv[1]):
+    count_and_print(sys.argv[1])
+elif len(sys.argv) >= 2:
+    for passedParameter in sys.argv[1:]:
+        if os.path.isfile(passedParameter):
+            count_and_print(passedParameter)
+        else:
+            print("Passed parameter is not a file")
