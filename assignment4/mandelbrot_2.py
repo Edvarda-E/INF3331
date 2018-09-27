7e8e1d07d5e80cedcb665dc9766b73cc9b38f769
Edvarda-E <edvarda.eriksen@outlook.com>
2018-09-27 14:58:15 +0200

    Implemented saving of images

--- assignment4/mandelbrot_2.py
+++ assignment4/mandelbrot_2.py
@@ -64,12 +64,14 @@ def main():
     colours = [(0.1411764705882353, 0.1607843137254902, 0.1803921568627451),    # grey - rgb(36, 41, 46)
                (0.11372549019607843, 0.7254901960784313, 0.32941176470588235)]  # green - rgb(29, 185, 84)
     custom_colourmap = LinearSegmentedColormap.from_list("gray_to_green", colours, 1000)
-    # mandelbrot_matrix = draw_mandelbrot_set(-0.74877, -0.74872, 0.06505, 0.06510, 1000, 1000, 1000)
-    mandelbrot_matrix = draw_mandelbrot_set(-2.0, 0.5, -1.25, 1.25, 1000, 1000, 80)
-    extent = [-2.0, 0.5, -1.25, 1.25]
+    mandelbrot_matrix = draw_mandelbrot_set(-0.74877, -0.74872, 0.06505, 0.06510, 1000, 1000, 1000)
+    extent = [-0.74877, -0.74872, 0.06505, 0.06510]
+    # mandelbrot_matrix = draw_mandelbrot_set(-2.0, 0.5, -1.25, 1.25, 1000, 1000, 80)
+    # extent = [-2.0, 0.5, -1.25, 1.25]
     plt.imshow(mandelbrot_matrix, cmap=custom_colourmap, extent=extent)
+    plt.savefig("image_1.png")
     print(datetime.now() - startTime)
-    plt.show()
+    #plt.show()
 
 
 # Runs if mandelbrot_1.py is called directly from commandline
