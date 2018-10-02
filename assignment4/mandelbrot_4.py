406f3363651a40fa1f8379031219a463512e4045
Edvarda-E <edvarda.eriksen@outlook.com>
2018-10-02 22:53:20 +0200

    Added reprot for task 4.4

--- assignment4/mandelbrot_4.py
+++ assignment4/mandelbrot_4.py
@@ -1,4 +1,5 @@
 from mandelbrot_cython import mandelbrot_set
+from datetime import datetime
 
 
 def main(xmin, xmax, ymin, ymax, width, height, image_name, max_iterations=1000):
@@ -7,4 +8,8 @@ def main(xmin, xmax, ymin, ymax, width, height, image_name, max_iterations=1000)
 
 # Runs if mandelbrot_1.py is called directly from commandline
 if __name__ == "__main__":
-    main(-0.74877, -0.74872, 0.06505, 0.06510, 1000, 1000, "cython_image", 1000)
+    startTime = datetime.now()
+    print("Program started")
+    main(-2.0, 0.5, -1.25, 1.25, 1000, 1000, "cython_image", 80)
+    # main(-0.74877, -0.74872, 0.06505, 0.06510, 1000, 1000, "cython_image", 1000)
+    print(datetime.now() - startTime)
