3da90d3c3e2eaa0d4dfd13a1320cfe180c1137d5
Edvarda <edvarda.eriksen@outlook.com>
2018-11-01 21:55:21 +0100

    Refactor in progress

--- assignment5/highlighter.py
+++ assignment5/highlighter.py
@@ -4,6 +4,33 @@ import re
 from filereader import read_syntax_file, read_color_file
 
 
+def color_injection(match, color):
+    print("---------")
+    print(match, color)
+    return "Hello"
+
+
+def highlight(input, regex_dict, theme_dict=None):
+    """
+
+    :param input:
+    :param regex_dict:
+    :param theme_dict:
+    :return:
+    """
+    end_color = r'\033[0m'
+    match_string_array = [None] * len(input)
+    print("::::::::")
+    for regex, regex_name in regex_dict.items():
+        # matcher = re.compile(regex.strip('\"'))  # Remove the " from the RegEx strings
+        for color_name, color_theme in theme_dict.items():
+            if regex_name in theme_dict and color_name == regex_name:
+                re.sub(r'{}'.format(regex.strip('\"')), lambda match, color=color_theme: color_injection(match, color), input, flags=re.MULTILINE)
+
+
+
+
+
 def main():
     parser = argparse.ArgumentParser(description="Highlight parts of an output file in the terminal using a syntax "
                                                  "dictionary, a theme dictionary and a file to apply them to")
@@ -24,6 +51,8 @@ def main():
     if os.path.isfile(args.syntax) and os.path.isfile(args.theme) and os.path.isfile(args.output_file):
         regex_dict = read_syntax_file(args.syntax)
         color_dict = read_color_file(args.theme)
+        output_string = open(args.output_file, 'r').read()
+        highlight(output_string, regex_dict, color_dict)
         # print(regex_dict, color_dict)
         match_string=''
         for regex, name in regex_dict.items():
