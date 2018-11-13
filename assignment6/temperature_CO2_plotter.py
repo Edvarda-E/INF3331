6eed1ec162d0c28951713ae160be9ab41c45463b
Edvarda <edvarda.eriksen@outlook.com>
2018-11-13 14:11:37 +0100

    Set up files and directories needed for 6.1

---
+++ assignment6/temperature_CO2_plotter.py
@@ -0,0 +1,32 @@
+
+
+def plot_temperature(month, ymin, ymax, temperature_data):
+    """"
+    Module that generates a plot of the temperature data provided over the month given.
+
+    Arguments:
+        month (str): What month to display temperature data for
+        ymin (int): Smallest value for the y-axis
+        ymax (int): Largest value for the y-axis
+        temperature_data (dict): Temperature data to be plotted
+
+    Returns:
+        A plot of the given data
+    """
+    pass
+
+
+def plot_CO2(time_range, ymin, ymax, plot_data):
+    """
+    Module that generates a plot of the CO2 data provided over the time range given.
+
+    Arguments:
+        time_range (tuple): Start and end year for plot
+        ymin (int): Smallest value for the y-axis
+        ymax (int): Largest value for the y-axis
+        temperature_data (dict): CO2 data to be plotted
+
+    Returns:
+        A plot of the given data
+    """
+    pass
